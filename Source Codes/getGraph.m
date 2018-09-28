3062eb90918fb51a3c8ceeb64062d06695181168
caryquibada <cmquibada@up.edu.ph>
2018-09-29 06:05:27 +0800

    Revert "Codes and initial results for tip decomp for Blog Catalog 3 dataset"t
    This reverts commit f36d5ec507adb6258f096fb231806baf800a1f7f.

--- Source Codes/getGraph.m
+++ Source Codes/getGraph.m
@@ -1,21 +1,23 @@
 function G = getGraph(filename,threshold)
 
-myValues=gpuArray(csvread(filename,0,0));  %Reading the edges file
-myValues = gpuArray(unique(sort(myValues,2),'rows'));
+myValues=csvread(filename,0,0);  %Reading the edges file
+myValues = unique(sort(myValues,2),'rows');
 [c,d]=size(myValues);
 a={};
 b={};
 start=tic;
 parfor i=1:c
+    disp(i);
     a(i)={num2str(myValues(i,1))};
     b(i)={num2str(myValues(i,2))};
 end
 timeout=toc(start)
 G=graph(a,b); %Constructing the graph
 [n l]=size(G.Nodes);
-thresholds=gpuArray(zeros(n));
+thresholds=zeros(1,n);
 start=tic;
-for i=1:n
+parfor i=1:n
+    disp(i);
     currNode=degree(G,G.Nodes.Name(i));
     if(currNode<threshold)
         thresholds(i)=currNode;
@@ -24,4 +26,4 @@ for i=1:n
     end
 end
 out=toc(start)
-G.Nodes.Thresholds=thresholds;
+G.Nodes.Thresholds=thresholds';
