d2ba0f81221bd40dcb3f92aa89f4760ffa9494fc
caryquibada <cmquibada@up.edu.ph>
2018-09-28 16:32:37 +0800

    added data sets getGraph is parallel and construct graph

--- Source Codes/TIPDecomp.m
+++ Source Codes/TIPDecomp.m
@@ -1,30 +1,32 @@
 function G=TIPDecomp(G)
 n=size(G.Nodes,1);
+ctr=n;
 dist=zeros(n,1);
 for i=1:n
-    dist(i)=degree(G,i)-G.Nodes.Thresholds(i);
+    dist(i)=degree(G,G.Nodes.Name(i))-G.Nodes.Thresholds(i);
+    disp(i);
 end
 G.Nodes.dist=dist;
 flag=true;
+
 while flag==true
     vi=min(G.Nodes.dist);
     currV=find(G.Nodes.dist==vi);
-    currVid=find(G.Nodes.Label==string(currV(1)));
-    fprintf('V_i: %g, Threshold of V_i: %g, Graph Size: %g\n',currVid,G.Nodes.Thresholds(currVid),size(find(G.Nodes.Status==0),1));
-    if G.Nodes.dist(currVid)==inf
+    ctr=ctr-1;
+    if G.Nodes.dist(currV(1))==inf
         return
     else
-        neighborsV=neighbors(G,currVid);
+        neighborsV=neighbors(G,G.Nodes.Name(currV(1)));
         m=size(neighborsV,1);
         for i=1:m
-            currN=find(G.Nodes.Label==string(neighborsV(i)));
-            if G.Nodes.dist(currN)>0 && G.Nodes.Status(currN)==0
+            currN=findnode(G,neighborsV(i));
+            if G.Nodes.dist(currN)>0
                 G.Nodes.dist(currN)=G.Nodes.dist(currN)-1;
-            elseif G.Nodes.dist(currN)<=0 && G.Nodes.Status(currN)==0
+            elseif G.Nodes.dist(currN)<=0
                 G.Nodes.dist(currN)=inf;
             end
         end
     end
-    G.Nodes.dist(currVid)=inf;
-    G.Nodes.Status(currVid)=1;
+    G=rmnode(G,G.Nodes.Name(currV(1)));
+    disp(size(G.Nodes));
 end
--- Source Codes/runTIP.m
+++ Source Codes/runTIP.m
@@ -1,9 +1,12 @@
-function result = runTIP(filename)
-
+function result = runTIP
+filename="LiveMocha.csv";
+tipOutput=fopen('TIPOutput.txt','w');
 thresholds=[10,9,8,7,6,5,4,3,2,1];
 result=[];
 for i=1:10
     G=constructGraph(filename,thresholds(i));
     resultG=TIPDecomp(G);
     result=[result,size(find(resultG.Nodes.Status==0),1)];
+    fprintf(tipOutput,"%g\n",size(find(resultG.Nodes.Status==0),1));
 end
+fclose(tipOutput);
