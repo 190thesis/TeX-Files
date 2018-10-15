e112f664ef8f2505bc9d4805fbe6c9424f9679be
Patricio Casurao <ptcasurao@gmail.com>
2018-10-15 15:09:45 +0800

    check if all are activated

---
+++ Source Codes/Propagate.m
@@ -0,0 +1,38 @@
+function active = Propagate(P,G)
+Q=[];
+Q=[Q ;P'];
+[n,~]=size(G.Nodes);
+for i=1:size(P)
+    G.Nodes.Threshold(P(1))=0;
+end
+while ~isempty(Q)
+   T=Q(1);
+   Q=Q(2:size(Q));
+   tid=T;
+   G.Nodes.Status(tid)=1;
+%    disp(Q);
+   disp(T);
+    neighborsT=neighbors(G,tid);
+    [ntsize,~]=size(neighborsT);
+    for i=1:ntsize
+       G.Nodes.Thresholds(neighborsT(i))=max(0, G.Nodes.Thresholds(neighborsT(i))-1);
+       if G.Nodes.Thresholds(neighborsT(i))==0 && G.Nodes.Status(neighborsT(i))==0
+           qcontains=false;
+           for k=1:size(Q)-1
+               if Q(k)==neighborsT(i)
+                   qcontains=true;
+               end
+           end
+           if qcontains==false
+               Q=[Q ;neighborsT(i)];
+           end
+       end
+    end
+    
+end
+active = true;
+    for i=1:n
+        if G.Nodes.Status(i)==0
+            active=false;
+        end
+    end
